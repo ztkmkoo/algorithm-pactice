8287f09ad660dca2bd41e135d146a301ecbdb139
ztkmkoo <ztkmk@naver.com>
2019-07-27 18:35:04 +0900

    # add hacker rank practice

--- src/main/java/com/ztkmkoo/hackerrank/HackerRankPractice.java
+++ src/main/java/com/ztkmkoo/hackerrank/HackerRankPractice.java
@@ -2,6 +2,10 @@ package com.ztkmkoo.hackerrank;
 
 import com.ztkmkoo.practice.Practice;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public abstract class HackerRankPractice implements Practice {
 
     private final String[][] inputs;
@@ -10,23 +14,52 @@ public abstract class HackerRankPractice implements Practice {
         this.inputs = inputs;
     }
 
-    public abstract Object run(Object[] params);
-
-    public abstract Object[] getPrams(final String[] input);
+    public abstract Object run(final List<String[]> input);
 
 
     @Override
     public final void run() {
 
-        if (inputs.length == 0) {
+        if (inputs == null || inputs.length == 0) {
             run(null);
             return;
         }
 
-        for (int i = 0; i < inputs.length; i++) {
+        final int size = inputs.length;
+
+        for (int i = 0; i < size; i++) {
             final String[] input = inputs[i];
-            final Object[] params = getPrams(input);
-            run(params);
+            final List<String[]> params = splitString(input);
+
+            final long start = System.currentTimeMillis();
+            final Object r = run(params);
+            final long cost = System.currentTimeMillis() - start;
+
+            if (r != null)
+                System.out.println(String.format("[TEST - %02d] %d ms: ", i + 1, cost) + r.toString());
+            else
+                System.out.println(String.format("[TEST - %02d] %d ms: ", i + 1, cost));
         }
+    }
+
+    protected List<String[]> splitString(String[] input) {
+
+        if (input == null || input.length == 0)
+            return Collections.emptyList();
+
+        final List<String[]> result = new ArrayList<>(input.length);
+
+        for (int i = 0; i < input.length; i++) {
+
+            final String str = input[i];
+            if (str == null || str.length() == 0) {
+                result.add(new String[0]);
+            } else {
+                final String[] splits = str.split(" ");
+                result.add(splits);
+            }
+        }
+
+        return result;
     }
 }
